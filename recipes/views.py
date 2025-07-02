df0cd008ffd5eee42a3e747a1daf515e986e7feb
filipe <solarcubix@gmail.com>
2025-07-02 14:33:03 -0300

    url and views fix

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
-
-def contact(request):
-    return render(request, 'contact.html')
-
-def about(request):
-    return HttpResponse("Hello, this is the about page! You imported it from recipes/views.py")
