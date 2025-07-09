5e33db7c172320cf2e60027ceb551c0353e2e153
Filipe <solarcubix@gmail.com>
2025-07-09 19:32:13 -0300

    feat: new page

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'pages\home.html')
+
+def recipe(request, id):
+    return render(request, 'pages\home.html')  
